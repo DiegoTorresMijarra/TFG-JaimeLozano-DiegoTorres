593a0045b89f93e7ab6a37379bf28d1a657ac98d
Diego Torres Mijarra <diego.torres@alumno.iesluisvives.org>
2024-05-11 12:36:36 +0200

    firstSteps
    JPA IN, init bbdd old data. App Prop updated

---
+++ backApp/database/init.sql
@@ -0,0 +1,29 @@
+SELECT 'CREATE DATABASE macjavaPostgres'
+WHERE NOT EXISTS (SELECT FROM pg_database WHERE datname = 'macjavaPostgres');
+
+CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
+
+DROP TABLE IF EXISTS "products";
+
+CREATE SEQUENCE products_id_seq INCREMENT 1 MINVALUE 1 MAXVALUE 9223372036854775807 START 6 CACHE 1;
+-- Crear la tabla products
+CREATE TABLE "public"."products"
+(
+    "id"        bigint DEFAULT nextval('products_id_seq') NOT NULL,
+    "nombre"    character varying(255),
+    "precio"    double precision,
+    "stock"     integer,
+    "gluten"    boolean,
+    "createdAt" timestamp,
+    "updatedAt" timestamp default CURRENT_TIMESTAMP,
+    "deletedAt" timestamp default null,
+    CONSTRAINT "products_pkey" PRIMARY KEY ("id")
+) WITH (oids = false);
+-- Insertar la tabla products
+INSERT INTO "products" ("id", "nombre", "precio", "stock", "gluten", "createdAt", "updatedAt")
+VALUES (1, 'Producto1', 10.50, 100, true, '2023-01-01', '2023-01-01'),
+       (2, 'Producto2', 15.75, 50, false, '2023-01-02', '2023-01-02'),
+       (3, 'Producto3', 20.00, 75, true,  '2023-01-03', '2023-01-03'),
+       (4, 'Producto4', 8.99, 120, false, '2023-01-04', '2023-01-04'),
+       (5, 'Producto5', 25.50, 200, true, '2023-01-05', '2023-01-05')
+;
