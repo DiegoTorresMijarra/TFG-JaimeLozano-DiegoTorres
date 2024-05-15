7d59dd0e80790635ee6c82fbf35976271e752cdb
Diego Torres Mijarra <diego.torres@alumno.iesluisvives.org>
2024-05-15 22:05:23 +0200

    restaurants: toma contacto common, actualizar controller y filter

---
+++ backApp/src/main/java/es/jaimelozanodiegotorres/backapp/rest/restaurants/controllers/RestaurantController.java
@@ -0,0 +1,180 @@
+package es.jaimelozanodiegotorres.backapp.rest.restaurants.controllers;
+
+import es.jaimelozanodiegotorres.backapp.pagination.PageResponse;
+import es.jaimelozanodiegotorres.backapp.rest.restaurants.dto.NewRestaurantDTO;
+import es.jaimelozanodiegotorres.backapp.rest.restaurants.dto.UpdatedRestaurantDTO;
+import es.jaimelozanodiegotorres.backapp.rest.restaurants.modelos.Restaurant;
+import es.jaimelozanodiegotorres.backapp.rest.restaurants.servicios.RestaurantServiceImpl;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.Parameters;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Optional;
+
+/**
+ * Controlador de la entidad Restaurant
+ * Anotamos la clase con @RestController para indicar que es un controlador
+ * @Parameter service Servicio de restaurantes
+ */
+@RestController
+@Tag(name = "Restaurantes", description = "Endpoint de Restaurantes de nuestra tienda")
+public class RestaurantController {
+    RestaurantServiceImpl service;
+
+    /**
+     * Constructor de la clase
+     * @param service Servicio de restaurantes
+     */
+    @Autowired
+    public RestaurantController(RestaurantServiceImpl service){
+        this.service=service;
+    }
+
+    /**
+     * Método que obtiene todos los restaurantes que cumplan con los parámetros de búsqueda
+     * @param name Opcional: nombre del restaurante
+     * @param number Opcional: número de teléfono del restaurante
+     * @param isDeleted Opcional: indica si el restaurante está eliminado
+     * @param page numero de página a recuperar
+     * @param size tamaño de la página
+     * @param sortBy campo por el que ordenar
+     * @param direction dirección de la ordenación
+     * @return ResponseEntity con una pagina de restaurantes que cumplan con los parámetros de búsqueda
+     */
+    @Operation(summary = "Obtiene todos los restaurantes que cumplan con los parámetros de búsqueda")
+    @Parameters({
+            @Parameter(name = "name", description = "Filtrar por nombre", example = "Restaurante"),
+            @Parameter(name = "number", description = "Filtrar por nº de teléfono", example = "123456789"),
+            @Parameter(name = "isDeleted", description = "Filtrar por restaurante eliminado", example = "false"),
+            @Parameter(name = "page", description = "Número de página a recuperar", example = "0"),
+            @Parameter(name = "size", description = "Tamaño de la página", example = "10"),
+            @Parameter(name = "sortBy", description = "Campo por el que ordenar", example = "id"),
+            @Parameter(name = "direction", description = "Dirección de la ordenación", example = "asc"),
+    })
+    @ApiResponses(value = {
+            @io.swagger.v3.oas.annotations.responses.ApiResponse(responseCode = "200", description = "Lista de restaurantes que cumplan con los parámetros de búsqueda"),
+            @io.swagger.v3.oas.annotations.responses.ApiResponse(responseCode = "400", description = "Error en los parámetros de búsqueda"),
+            @io.swagger.v3.oas.annotations.responses.ApiResponse(responseCode = "404", description = "Restaurante no encontrado"),
+    })
+    @GetMapping("/restaurant")
+    public ResponseEntity<PageResponse<Restaurant>> getRestaurants(
+            @RequestParam(required=false) Optional<String> name,
+            @RequestParam(required = false)Optional<String> number,
+            @RequestParam(defaultValue = "false",required = false)Optional<Boolean> isDeleted,
+            @RequestParam(defaultValue = "0") int page,
+            @RequestParam(defaultValue = "10") int size,
+            @RequestParam(defaultValue = "id") String sortBy,
+            @RequestParam(defaultValue = "asc") String direction
+            ){
+        Sort sort=direction.equalsIgnoreCase(Sort.Direction.ASC.name())? Sort.by(sortBy).ascending():Sort.by(sortBy).descending();
+        Page<Restaurant> pageResult= service.findAll(name, number,isDeleted, PageRequest.of(page,size,sort));
+        return ResponseEntity.ok().body(PageResponse.of(pageResult,sortBy,direction));
+    }
+
+    /**
+     * Obtiene un restaurante por su ID
+     * @param id ID del restaurante a buscar
+     * @return Restaurante que coincida con el ID
+     */
+    @Operation(summary = "Obtiene un restaurante por su ID")
+    @Parameter(name = "id", description = "ID del restaurante a buscar", example = "1")
+    @ApiResponses(value = {
+            @io.swagger.v3.oas.annotations.responses.ApiResponse(responseCode = "200", description = "Restaurante encontrado"),
+            @io.swagger.v3.oas.annotations.responses.ApiResponse(responseCode = "404", description = "Restaurante no encontrado"),
+    })
+    @GetMapping ("/restaurant/{id}")
+    public ResponseEntity<Restaurant> getRestaurant(@PathVariable Long id){
+        return ResponseEntity.ok(service.findById(id));
+    }
+
+    /**
+     * Guarda un restaurante
+     * @param restaurantDTO RestauranteDTO con la informacion del restaurante a guardar
+     * @return Restaurante guardado
+     */
+    @Operation(summary = "Guarda un restaurante con la información de un RestauranteDTO")
+    @Parameter(name = "restaurantDTO", description = "RestauranteDTO con información del restaurante a guardar", required = true)
+    @ApiResponses(value = {
+            @io.swagger.v3.oas.annotations.responses.ApiResponse(responseCode = "201", description = "Restaurante guardado"),
+            @io.swagger.v3.oas.annotations.responses.ApiResponse(responseCode = "400", description = "Error en los parámetros del Restaurante"),
+            @io.swagger.v3.oas.annotations.responses.ApiResponse(responseCode = "401", description = "No autorizado")
+    })
+    @io.swagger.v3.oas.annotations.parameters.RequestBody(description = "Restaurante a guardar", required=true)
+    @PostMapping("/restaurant")
+   // @PreAuthorize("hasRole('ADMIN')")
+    public ResponseEntity<Restaurant> createRestaurant(@Valid @RequestBody NewRestaurantDTO restaurantDTO){
+        return ResponseEntity.status(HttpStatus.CREATED).body(service.save(restaurantDTO));
+    }
+
+    /**
+     * Actualiza un restaurante
+     * @param id ID del restaurante a actualizar
+     * @param restaurantDTO RestauranteDTO con la información a actualizar
+     * @return Restaurante actualizado
+     */
+    @Operation(summary = "Actualiza un restaurante con la información de un RestauranteDTO")
+    @Parameters({
+            @Parameter(name = "id", description = "ID del restaurante a actualizar", example = "1",required = true),
+            @Parameter(name = "restaurantDTO", description = "RestauranteDTO con información actualizada", required = true)
+    })
+    @io.swagger.v3.oas.annotations.parameters.RequestBody(description = "Restaurante a actualizar", required=true)
+    @ApiResponses(value = {
+            @io.swagger.v3.oas.annotations.responses.ApiResponse(responseCode = "200", description = "Restaurante actualizado"),
+            @io.swagger.v3.oas.annotations.responses.ApiResponse(responseCode = "400", description = "Error en los parámetros del Restaurante"),
+            @io.swagger.v3.oas.annotations.responses.ApiResponse(responseCode = "404", description = "Restaurante no encontrado"),
+    })
+    @PutMapping("/restaurant/{id}")
+   // @PreAuthorize("hasRole('ADMIN')")
+    public ResponseEntity<Restaurant> updateRestaurant(@PathVariable Long id, @Valid @RequestBody UpdatedRestaurantDTO restaurantDTO){
+        return ResponseEntity.ok(service.updateByID(id, restaurantDTO));
+    }
+
+    /**
+     * Elimina un restaurante
+     * @param id ID del restaurante a eliminar
+     */
+    @Operation(summary = "Elimina un restaurante por su ID")
+    @Parameter(name = "id", description = "ID del restaurante a eliminar", example = "1",required = true)
+    @ApiResponses(value = {
+            @io.swagger.v3.oas.annotations.responses.ApiResponse(responseCode = "204", description = "Restaurante eliminado"),
+            @io.swagger.v3.oas.annotations.responses.ApiResponse(responseCode = "404", description = "Restaurante no encontrado"),
+            @io.swagger.v3.oas.annotations.responses.ApiResponse(responseCode = "401", description = "No autorizado")
+    })
+    @DeleteMapping("/restaurant/{id}")
+    //@PreAuthorize("hasRole('ADMIN')")
+    public ResponseEntity<Void> deleteRestaurant(@PathVariable Long id){
+        service.deleteById(id);
+        return ResponseEntity.noContent().build();
+    }
+
+    /**
+     * Controla las excepciones de validación
+     * @param excep Excepción de validación
+     * @return Mapa con los errores de validación
+     */
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public Map<String,String> handleValidationExceptions(MethodArgumentNotValidException excep){
+        Map<String,String> errors = new HashMap<>();
+        excep.getBindingResult().getAllErrors().forEach((error) -> {
+            String fieldName = ((FieldError) error).getField();
+            String errorMessage = error.getDefaultMessage();
+            errors.put(fieldName, errorMessage);
+        });
+        return errors;
+    }
+}
